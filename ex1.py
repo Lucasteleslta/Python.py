1e447e388b67e3d0f8e70dfb359f2226edbd550b
LUCAS TELES ALVES <112990396+Lucasteleslta@users.noreply.github.com>
2024-09-25 22:18:25 -0300

    Delete ex1.py

--- ex1.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input("Digite um numero inteiro:"))
-num2 = int(input("Digite um numero inteiro:"))
-
-print (f"o valor da soma é:", num1+num2)
-print (f"o valor da subtração é:", num1-num2)
-print (f"o valor da multiplicação é:", num1*num2)
-print (f"o valor da divisão é:", num1/num2) 
